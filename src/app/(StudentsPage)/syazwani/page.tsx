031c6a0cffdff313d6da21b53ac916ca25b9f4fc
AriiiiDkCode <m.harith.hadi@gmail.com>
2025-06-16 00:40:14 +0800

    cloud test

--- src/app/(StudentsPage)/syazwani/page.tsx
+++ src/app/(StudentsPage)/syazwani/page.tsx
@@ -8,14 +8,6 @@ import { ForthParallax } from "./components/forthparallax";
 import { Quizsection } from "./components/quizsection";
 
 const Page = () => {
-    useEffect(() => {
-        if (typeof window !== "undefined" && !sessionStorage.getItem("syazwaniRefreshed")) {
-          sessionStorage.setItem("syazwaniRefreshed", "true");
-          setTimeout(() => {
-            window.location.reload();
-          }, 2000); // 2 seconds
-        }
-      }, []);
 
     return (
         <>

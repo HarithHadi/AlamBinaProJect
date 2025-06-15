67155d1fd8bb277f66ed9129985026502cbd3dd2
AriiiiDkCode <m.harith.hadi@gmail.com>
2025-06-15 15:48:28 +0800

    removed overlap image

--- src/app/(StudentsPage)/syazwani/page.tsx
+++ src/app/(StudentsPage)/syazwani/page.tsx
@@ -4,29 +4,24 @@ import { Parallax } from "@/app/(StudentsPage)/syazwani/components/parallax";
 import { Secparallax } from "@/app/(StudentsPage)/syazwani/components/secparallax";
 import { Thirdparallax } from "./components/thirdparallax";
 import { ForthParallax } from "./components/forthparallax";
-import { Hero } from "@/components/heroMain";
-import { Map } from "@/components/ui/map";
-import OverlapImage from "./components/overlapImage";
-import Quiz from "./components/quiz";
+
 import { Quizsection } from "./components/quizsection";
 
 const Page = () => {
     useEffect(() => {
-        // Only refresh if not already refreshed
-        if (typeof window !== "undefined" && !localStorage.getItem("syazwaniRefreshed")) {
-            localStorage.setItem("syazwaniRefreshed", "true");
-            setTimeout(() => {
-                window.location.reload();
-            }, 2000); // 2 seconds
+        if (typeof window !== "undefined" && !sessionStorage.getItem("syazwaniRefreshed")) {
+          sessionStorage.setItem("syazwaniRefreshed", "true");
+          setTimeout(() => {
+            window.location.reload();
+          }, 2000); // 2 seconds
         }
-    }, []);
+      }, []);
 
     return (
         <>
             <div>
                 <Parallax />
-                <Secparallax />
-                {/* <OverlapImage />  */}
+                <Secparallax /> 
                 <ForthParallax />
                 <Thirdparallax />
                 <Quizsection />
